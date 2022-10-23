e8c624373bac7eec2358d718e8ba3c97a8c7b811
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-10-23 22:46:20 +0200

    add content to why the composition api

--- src/views/vue-composition-api/WhyTheCompositionApi.vue
+++ src/views/vue-composition-api/WhyTheCompositionApi.vue
@@ -1,23 +1,39 @@
+<script lang="ts">
+export default {
+  setup() {
+    const reasons = [
+      "Solve limitations of Vue 2.",
+      "Readability of components.",
+      "Code reuse patterns",
+      "Has TypeScript Suport",
+      "I can use Composition Functions outside of setep",
+      "Code can now be organized by logical concerns",
+      "We can avoid to use Mixins, Mixin Factories or Scoped Slots",
+      "We can use Composition Functions, wich are lesscode, familiar functions, extremely flexible and tooling friendly",
+      "We can still use the regular syntag with vue 3",
+    ];
+
+    return { reasons }
+  }
+};
+
+</script>
+
 <template>
   <div class="container px-4 py-5" id="hanging-icons">
-    <h2 class="pb-2 border-bottom">Hanging icons</h2>
+    <h2 class="pb-2 border-bottom">Why the composition api?</h2>
     <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-      <div v-for="n in 10" :key="n" class="col d-flex align-items-start">
+      <div v-for="reason, index in reasons" :key="index" class="col d-flex align-items-start">
         <div
           class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-binoculars"
-            viewBox="0 0 16 16">
-            <path
-              d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
+            class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
           </svg>
         </div>
         <div>
-          <h3 class="fs-2">Featured title</h3>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and
-            probably just keep going until we run out of words.</p>
-          <a href="#" class="btn btn-primary">
-            Primary button
-          </a>
+          <p>{{ reason }}</p>
         </div>
       </div>
     </div>
