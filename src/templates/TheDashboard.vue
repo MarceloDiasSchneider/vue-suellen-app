c67452a88a79b4a76609ff916f409dea8e3d31f4
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-10-29 22:10:44 +0200

    create template TheDashboard

---
+++ src/templates/TheDashboard.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  setup() { },
+};
+</script>
+
+<template>
+  <div class="h-100">
+    <main class="d-flex flex-nowrap h-100">
+      <slot name="sidebar"></slot>
+      <slot name="content"></slot>
+    </main>
+  </div>
+</template>
