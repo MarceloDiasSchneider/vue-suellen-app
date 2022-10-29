dc0dcb1a27bc5376ce61797d1863fd45b7f92a19
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-10-29 22:13:52 +0200

    create children routes why-the-composition-api

---
+++ src/router/why-the-composition-api-children.ts
@@ -0,0 +1,7 @@
+export const why_the_composition_api_children: Array<object> = [
+  {
+    path: "/why-the-composition-api",
+    name: "why-the-composition-api",
+    component: () => import("@/views/vue-composition-api/WhyTheCompositionApi.vue"),
+  },
+];
