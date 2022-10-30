146e068d2d0ca301a13a700e2b5d66723eba9b43
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-10-30 18:49:09 +0100

    routes why-the-composition-api  children add propriety link

--- src/router/why-the-composition-api-children.ts
+++ src/router/why-the-composition-api-children.ts
@@ -2,6 +2,7 @@ export const why_the_composition_api_children: Array<object> = [
   {
     path: "/why-the-composition-api",
     name: "why-the-composition-api",
+    link: "Why the composition api",
     component: () => import("@/views/vue-composition-api/WhyTheCompositionApi.vue"),
   },
 ];
