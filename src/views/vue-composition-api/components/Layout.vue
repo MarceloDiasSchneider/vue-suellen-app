75758972c97e35f91094c5cfd3f1c3e2d8cca73d
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-11-01 13:34:22 +0100

    create vue composition api component layoput

---
+++ src/views/vue-composition-api/components/Layout.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import TheDashboard from '@/templates/TheDashboard.vue';
+import TheSidebarVue from '@/templates/TheSidebar.vue';
+import { why_the_composition_api_children } from '@/router/why-the-composition-api-children'
+
+export default {
+  setup() { return { why_the_composition_api_children } },
+  components: {
+    TheDashboard,
+    TheSidebarVue,
+  }
+};
+
+</script>
+
+<template>
+  <TheDashboard>
+    <template #sidebar>
+      <TheSidebarVue :links="why_the_composition_api_children" />
+    </template>
+    <template #content>
+      <slot name="content"></slot>
+    </template>
+  </TheDashboard>
+</template>
