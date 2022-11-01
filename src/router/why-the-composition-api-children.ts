2704ea63d73af8fe3d009cc34b992b4d2a09a17d
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-11-01 13:35:27 +0100

    routes add setup and reactive references

--- src/router/why-the-composition-api-children.ts
+++ src/router/why-the-composition-api-children.ts
@@ -5,4 +5,10 @@ export const why_the_composition_api_children: Array<object> = [
     link: "Why the composition api",
     component: () => import("@/views/vue-composition-api/WhyTheCompositionApi.vue"),
   },
+  {
+    path: "/setup-and-reactive-references",
+    name: "setup-and-reactive-references",
+    link: "Setup and reactive references",
+    component: () => import("@/views/vue-composition-api/SetupAndReactiveReferences.vue"),
+  },
 ];
