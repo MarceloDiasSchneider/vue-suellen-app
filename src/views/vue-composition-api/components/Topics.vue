6d240dbb2699ac90faf45485bfe9760eb911e6b7
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-11-05 17:05:54 +0100

    create tipics component

---
+++ src/views/vue-composition-api/components/Topics.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import ArrowRightCircle from '@/assets/images/icons/ArrowRightCircle.vue'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    topics: {
+      type: Array,
+      required: false
+    },
+  },
+  components: {
+    ArrowRightCircle,
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h2 class="pb-2 border-bottom">{{ title }}</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div v-for="topci, index in topics" :key="index" class="col d-flex align-items-start">
+        <ArrowRightCircle />
+        <p>{{ topci }}</p>
+      </div>
+    </div>
+  </div>
+</template>
